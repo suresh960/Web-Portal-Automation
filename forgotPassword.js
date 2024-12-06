61aab6dc0ada59e1732267f973ab366fe296024c
suresh panwar <panwarsuresh289@gmail.com>
2024-12-06 18:18:50 +0530

    Added console log

--- forgotPassword.js
+++ forgotPassword.js
@@ -27,6 +27,7 @@ async function forgotPassword() {
 
         const backTologinButton = await driver.findElement(By.className('inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:bg-neutral-300 dark:ring-offset-gray-950 dark:focus-visible:ring-gray-300 border border-gray-200 bg-white hover:bg-gray-100 hover:text-gray-900 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 h-10 px-4 py-2 flex gap-2 items-center w-full'))
         await backTologinButton.click();
+        console.log('successfully back to Login')
 
         // Wait for the next page to load (adjust the selector to a post-login element)
         await driver.wait(until.titleContains('AR Genie Portal'), 5000);
